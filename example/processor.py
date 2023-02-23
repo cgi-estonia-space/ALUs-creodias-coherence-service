1340523adf93737359a52f9765af056edbcd6ddb
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-02-23 13:13:59 +0200

    Finishing touches for the processor for preliminary demo.

--- example/processor.py
+++ example/processor.py
@@ -1,12 +1,15 @@
 import glob
 import os
 import typer
-from structlog import get_logger
+import structlog
 from pathlib import Path
 
 from eof.download import find_unique_safes
+import structlog_cloudferro
 
-log = get_logger()
+
+structlog_cloudferro.setup_logging()
+log = structlog.getLogger("app")
 runner_status_offset = 100
 
 
@@ -31,24 +34,27 @@ def main(
     dem_args = ["--dem " + x + " " for x in dem_files]
     dem_args = " ".join(dem_args)
     command = f"alus-coh -r {safe_files_list[0].filename} -s {safe_files_list[1].filename} --orbit_dir {input_dir} \
-              -p {polarization[0]} --no_mask_cor -o {output_dir} {dem_args}"
+              -p {polarization[0]} --no_mask_cor -o {output_dir} {dem_args} --log_format_creodias"
+    log.info(command)
     res_val = os.system(command)
     if res_val != 0:
         raise typer.Exit(res_val)
 
+    log.info("Converting result to COG")
     results = glob.glob(os.path.join(output_dir, "S1*.tif"))
     output_filename = results[0]
     command = f"gdaladdo -r average {output_filename} 2 4 8 16"
-    res_val = os.system(command)
+    res_val = os.system(command + " > /dev/null 2>&1")
     if res_val != 0:
         raise typer.Exit(res_val)
 
     cog_filename = output_filename[:-4] + "_cog.tif"
     command = f"gdal_translate {output_filename} {cog_filename} \
                 -co COMPRESS=LZW -co TILED=YES -co COPY_SRC_OVERVIEWS=YES"
-    res_val = os.system(command)
+    res_val = os.system(command + " > /dev/null 2>&1")
     if res_val != 0:
         raise typer.Exit(res_val)
+    log.info("Final result - " + cog_filename)
 
 
 if __name__ == "__main__":
